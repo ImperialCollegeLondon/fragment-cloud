84b8f4de263a0708728142cb21508953c409554f
Dominic Schwarz <dominic.schwarz@gmx.ch>
2020-09-28 14:52:13 +0100

    fixed 2 typos in docstrings

--- include/atmospheric_density.hpp
+++ include/atmospheric_density.hpp
@@ -20,7 +20,7 @@ public:
     AtmosphericDensity(std::vector<double>&& height, const std::vector<double>& density);
 
     /**
-     * @brief calculate air density at height h, in [m/s]
+     * @brief calculate air density at height h, in [kg/m^3]
      * 
      * Extrapolates exponentially based on scale height at the edge of the height vector provided
      * 
@@ -31,7 +31,7 @@ public:
     /**
      * @brief calculate scale height H at height h
      * 
-     * Value H for which, at height h, the atmoshperic density is best approximated
+     * Value H for which, around height h, the atmoshperic density is best approximated
      * by rho(h) = rho_0 * exp(-h / H) for some value rho_0
      * 
      * @param h : height above sea level, in [m]
