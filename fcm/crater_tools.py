22e8a635a595692a434a1a0486f1b8bf4cffdb16
Dominic Schwarz <dominic.schwarz@gmx.ch>
2020-11-13 18:45:03 +0100

    - changed crater_tools.ellipse function    to no longer swap x and y axes - crater_tools: added unit tests and docstrings

--- fcm/crater_tools.py
+++ fcm/crater_tools.py
@@ -1,12 +1,47 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist
 from matplotlib.patches import Circle, Ellipse
 from sklearn.decomposition import PCA
 from sklearn.utils import resample
 
+from fcm._fcm_class import _check_number
 
-def plot_craters(craters, circle_color="black", ellipse_color=None, figsize=None):
+
+def plot_craters(craters, crater_color="black", ellipse_color=None, figsize=None):
+    """Make a matplotlib plot of a crater cluster.
+    Optionally computes and plots best-fit ellipse around cluster
+    
+    Parameters
+    ----------
+    craters : pandas.DataFrame
+        list of craters in cluster
+        columns = ['x', 'y', 'r']
+        units must be the same for all columns, typically meters
+    
+    crater_color : str, optional
+        color of circles representing craters
+            a Matplotlib color, see
+            https://matplotlib.org/gallery/color/named_colors.html?highlight=list%20named%20colors
+        default: black
+    
+    ellipse_color : str, optional
+        color of best-fit ellipse; a Matplotlib color
+        If specified, best-fit ellipse is computed and drawn
+    
+    figsize : tuple[int], optional
+        size of figure in inches
+
+    Returns
+    -------
+    matplotlib.figure.Figure
+
+    """
+    assert isinstance(craters, pd.DataFrame),\
+        "craters must be a pandas.DataFrame instance"
+    assert pd.Index(["x", "y", "r"]).isin(craters.columns).all(),\
+        "craters must have columns 'x', 'y', and 'r'"
     x_min = (craters.x - craters.r).min()
     x_max = (craters.x + craters.r).max()
     y_min = (craters.y - craters.r).min()
@@ -17,22 +52,22 @@ def plot_craters(craters, circle_color="black", ellipse_color=None, figsize=None
     y_min, y_max = y_min - margin * (y_max - y_min), y_max + margin * (y_max - y_min)
     
     fig, ax = plt.subplots(1, 1, figsize=figsize)
-    ax.set_ylim(x_min, x_max)
-    ax.set_xlim(y_min, y_max)
+    ax.set_xlim(x_min, x_max)
+    ax.set_ylim(y_min, y_max)
     
     for row in craters.itertuples():
-        circle = Circle((row.y, row.x), row.r, color=circle_color, fill=False)
+        circle = Circle((row.x, row.y), row.r, color=crater_color, fill=False)
         ax.add_artist(circle)
     
-    ax.set_xlabel('y')
-    ax.set_ylabel('x')
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
     ax.set_aspect('equal')
     
     if ellipse_color is not None:
         _, center, components = ellipse(craters)
-        ell = Ellipse((center[1], center[0]),
-                      np.linalg.norm(components[0]), np.linalg.norm(components[1]),
-                      180/np.pi * np.arctan2(components[0, 1], components[0, 0]),
+        ell = Ellipse((center[0], center[1]),
+                      np.linalg.norm(components[:, 0]), np.linalg.norm(components[:, 1]),
+                      180/np.pi * np.arctan2(components[1, 0], components[0, 0]),
                       color=ellipse_color, fill=False)
         ax.add_artist(ell)
     
@@ -40,13 +75,70 @@ def plot_craters(craters, circle_color="black", ellipse_color=None, figsize=None
 
 
 def effective_diameter(radii):
+    """Calculate effective diameter of a crater cluster
+    
+    d_eff = (sum of radii^3)^1/3
+    
+    Parameters
+    ----------
+    radii : numpy.ndarray
+        numpy array with radii of craters in the cluster
+        If array is more than 1-dimensional, computes effective diameter over last axis
+    
+    Returns
+    -------
+    Union[float, numpy.ndarray]
+        If input is 1-D, just the effective diameter (float)
+        If input is n-D, (numpy.ndarray) with n-1 dimensions; data = effective radii
+    """
     
     return 2 * np.cbrt((radii**3).sum(axis=-1))
 
 
-def ellipse(craters, bootstrap_samples=300, seed=6549810):
-
-    if craters.shape[0] > 5:
+def ellipse(craters, min_craters=5, bootstrap_samples=80, seed=74527):
+    """Calculate best-fit ellipse around crater cluster with PCA,
+    if cluster has at least min_craters.
+    Otherwise, just places a circle in the centre with diameter = effective diameter.
+    Performs bootstrapping to minimize influence of outliers.
+    
+    Parameters
+    ----------
+    craters : pandas.DataFrame
+        has columns [x, y, r]
+        list of x, y coordiantes and radii of craters in cluster
+    
+    min_craters : int, optional
+        minimum number of craters in a cluster where ellipse is calculated
+        min_craters >= 2
+        default = 5
+    
+    bootstrap_samples : int, optional
+        number of repetitions with bootstrapped samples from crater cluster
+        bootstrap_samples >= 1
+        default = 80
+    
+    seed : int, optional
+        seed for the random number generator used for bootstrapping
+        
+    Returns
+    -------
+    aspect : float
+        aspect ratio of best-fit ellipse
+        1 = circle, 0 = line
+        eccentricity = sqrt(1 - aspect^2)
+    
+    center : numpy.array([x, y])
+        coordinates of ellipse center
+    
+    components : np.array([x1, x2], [y1, y2])
+        components of semi-major (1) and semi-minor (2) axis
+    """
+    min_craters = _check_number(min_craters, "min_craters", False, 2, True, bound_format="{:d}")
+    bootstrap_samples = _check_number(bootstrap_samples, "bootstrap_samples", False, 1, True,
+                                      bound_format="{:d}")
+    seed = _check_number(seed, "seed", False, 0, True, bound_format="{:d}")
+    
+    if craters.shape[0] >= min_craters:
         coordinates = craters[['x', 'y']].to_numpy()
         
         pca = PCA(2)
@@ -58,16 +150,26 @@ def ellipse(craters, bootstrap_samples=300, seed=6549810):
             for _ in range(bootstrap_samples):
                 pca.fit(resample(coordinates, replace=True, random_state=rs))
                 explained_variances.append(np.sqrt(pca.explained_variance_))
-                pca_main_angles.append(np.arctan2(pca.components_[0, 0], pca.components_[0, 1]))
+                pca_main_angles.append(np.arctan2(pca.components_[0, 1], pca.components_[0, 0]))
         else:
             pca.fit(coordinates)
             explained_variances.append(np.sqrt(pca.explained_variance_))
-            pca_main_angles.append(np.arctan2(pca.components_[0, 0], pca.components_[0, 1]))
+            pca_main_angles.append(np.arctan2(pca.components_[0, 1], pca.components_[0, 0]))
         
         center = coordinates.mean(axis=0)
         explained_variances = np.array(explained_variances)
+        # calculate median aspect ratio
         aspect = np.median(explained_variances[:, 1] / explained_variances[:, 0])
         
+        # bring all angles into same half of the circle (around 0 or pi/2, depending)
+        pca_main_angles = np.array(pca_main_angles)
+        if ((np.pi/4 < np.abs(pca_main_angles)) & (3*np.pi/4 > np.abs(pca_main_angles))).mean() > 0.5:
+            pca_main_angles[pca_main_angles < 0] += np.pi
+        else:
+            pca_main_angles[pca_main_angles > np.pi/2] -= np.pi
+            pca_main_angles[pca_main_angles < -np.pi/2] += np.pi
+        
+        # calcualte median angle of main component
         angle = np.median(pca_main_angles)
         components = np.array([[np.cos(angle), -aspect * np.sin(angle)],
                                [np.sin(angle), aspect * np.cos(angle)]])
@@ -88,6 +190,19 @@ def ellipse(craters, bootstrap_samples=300, seed=6549810):
 
 
 def dispersion(craters):
+    """Calculate pairwise distane of crater centers
+    
+    Parameters
+    ----------
+    craters : pandas.DataFrame
+        has columns [x, y]
+        list of x, y coordiantes of craters in a cluster
+    
+    Returns
+    -------
+    numpy.ndarray
+        1D numpy array with pairwise distances, same unit as input units
+    """
     coordinates = craters[['x', 'y']].to_numpy()
     distances = pdist(coordinates)
     if distances.size == 0:
@@ -97,20 +212,80 @@ def dispersion(craters):
     
 
 def n_larger_D_max_frac(radii, frac=0.25):
+    """Count the number of craters in a cluster that are larger than
+    a fraction of the size of the largest crater in the cluster
     
-    r_max = radii.max()
+    Parameters
+    ----------
+    radii : numpy.ndarray
+        array of radii of craters in cluster
+        If array is more than 1-dimensional, counts over last axis
     
-    return (radii > r_max*frac).sum(axis=-1)
+    frac : float, optional
+        size fraction above which crater is counted
+        0 <= frac <= 1
+        default = 0.25
+    
+    Returns
+    -------
+    Union[int, numpy.ndarray]
+        If input is 1D, just the number of large craters
+        If input is nD, (numpy.ndarray) with n-1 dimensions; data = number of large craters
+    """
+    frac = _check_number(frac, "frac", True, 0, True, 1, True)
+    r_max = radii.max(axis=-1)
+    comp = np.repeat(r_max.reshape(radii.shape[:-1] + (1,)), radii.shape[-1], -1)
+    
+    return (radii > comp*frac).sum(axis=-1)
     
 
 def f_larger_D_max_frac(radii, frac=0.25):
+    """Compute the fraction of craters in a cluster that are larger than
+    a fraction of the size of the largest crater in the cluster
     
-    r_max = radii.max()
+    Parameters
+    ----------
+    radii : numpy.ndarray
+        array of radii of craters in cluster
+        If array is more than 1-dimensional, computes over last axis
     
-    return (radii > r_max*frac).mean(axis=-1)
+    frac : float, optional
+        size fraction above which crater is counted
+        0 <= frac <= 1
+        default = 0.25
+    
+    Returns
+    -------
+    Union[float, numpy.ndarray]
+        If input is 1D, just the number of large craters
+        If input is nD, (numpy.ndarray) with n-1 dimensions; data = number of large craters
+    """
+    frac = _check_number(frac, "frac", True, 0, True, 1, True)
+    r_max = radii.max(axis=-1)
+    comp = np.repeat(r_max.reshape(radii.shape[:-1] + (1,)), radii.shape[-1], -1)
+    
+    return (radii > comp*frac).mean(axis=-1)
 
 
 def lon_lat_to_meters(craters, Rp):
+    """Convert crater cluster with crater coordinates in (latitude, longitude), in degrees,
+    to meters distance from the cluster center
+    
+    Adds/overwrites columns [x, y] in the craters parameter
+    
+    Parameters
+    ----------
+    craters : pandas.DataFrame
+        has columns [lat, lon]
+        latitude and longitude of craters in a cluster, in degrees
+        
+    Rp : float
+        radius of the planet, in meters
+        Rp > 0
+    """
+    Rp = _check_number(Rp, "Rp", True, 0)
+    if not pd.Index(["lat", "lon"]).isin(craters.columns).all():
+        raise KeyError("craters must have columns lat ('latitude') and lon ('longitude')")
     
     lon_center, lat_center = craters.lon.mean(), craters.lat.mean()
     craters['x'] = Rp * (craters.lat - lat_center) * np.pi / 180
---
+++ tests/python/crater_tools_tests.py
@@ -0,0 +1,122 @@
+import os, sys
+THIS_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(THIS_DIR, "..", ".."))
+
+import numpy as np
+import pandas as pd
+from fcm import crater_tools
+
+
+def test_effective_diameter():
+    
+    dummy_craters = np.array([[1, 2, 3], [10, 20, 30]])
+    d_eff = crater_tools.effective_diameter(dummy_craters[0])
+    np.testing.assert_allclose(d_eff, 6.6038545)
+    
+    d_eff_2 = crater_tools.effective_diameter(dummy_craters)
+    np.testing.assert_allclose(d_eff_2, [d_eff, 10*d_eff])
+    
+    dummy_craters = pd.read_csv(os.path.join(THIS_DIR, "sample_cluster.csv"), index_col=0)
+    d_eff = crater_tools.effective_diameter(dummy_craters.r.to_numpy())
+    np.testing.assert_allclose(d_eff, 9.884990621782576)
+    
+
+def test_dispersion():
+    
+    dummy_craters = pd.DataFrame({"x": [-1, 1, -1, 1, 0], "y": [1, 1, -1, -1, 0]})
+    disp = crater_tools.dispersion(dummy_craters)
+    value_counts = pd.Series(disp).value_counts()
+    
+    expected = pd.Series({np.sqrt(2): 4, 2.0: 4, 2*np.sqrt(2): 2})
+    pd.testing.assert_series_equal(value_counts, expected)
+    
+    dummy_craters = pd.read_csv(os.path.join(THIS_DIR, "sample_cluster.csv"), index_col=0)
+    disp = crater_tools.dispersion(dummy_craters.iloc[:3])
+    np.testing.assert_allclose(np.sort(disp), [13.06700178, 25.58476293, 36.78969209])
+    
+
+def test_ellipse():
+    
+    dummy_craters = pd.read_csv(os.path.join(THIS_DIR, "sample_cluster.csv"), index_col=0)
+    aspect, center, components = crater_tools.ellipse(dummy_craters, bootstrap_samples=100,
+                                                      seed=123)
+    np.testing.assert_allclose(aspect, 0.3154624369511341)
+    np.testing.assert_allclose(center, [0, 0], atol=1e-8)
+    np.testing.assert_allclose(components,
+                               [[-34.92610814, -88.91326344], [281.8505566, -11.01787519]])
+    
+    dummy_craters[['x', 'y']] = dummy_craters[['y', 'x']]
+    aspect2, center2, components2 = crater_tools.ellipse(dummy_craters, bootstrap_samples=100,
+                                                         seed=123)
+    
+    np.testing.assert_allclose(aspect2, aspect)
+    np.testing.assert_allclose([center2[1], center2[0]], center)
+    np.testing.assert_allclose(np.abs([components2[1], components2[0]]), np.abs(components))
+
+
+def test_f_n_larger_d_max_frac():
+    
+    dummy_radii = np.array([[1, 3, 4, 1, 2], [4, 12, 16, 4, 8]])
+    
+    assert crater_tools.n_larger_D_max_frac(dummy_radii[0], 0.4) == 3
+    np.testing.assert_allclose(crater_tools.f_larger_D_max_frac(dummy_radii[0], 0.4), 0.6) 
+    np.testing.assert_allclose(crater_tools.n_larger_D_max_frac(dummy_radii, 0.4), [3, 3])
+    np.testing.assert_allclose(crater_tools.f_larger_D_max_frac(dummy_radii, 0.4), [0.6, 0.6])
+    
+    assert crater_tools.n_larger_D_max_frac(dummy_radii[0], 0.6) == 2
+    np.testing.assert_allclose(crater_tools.f_larger_D_max_frac(dummy_radii[0], 0.6), 0.4) 
+    np.testing.assert_allclose(crater_tools.n_larger_D_max_frac(dummy_radii, 0.6), [2, 2])
+    np.testing.assert_allclose(crater_tools.f_larger_D_max_frac(dummy_radii, 0.6), [0.4, 0.4])
+    
+
+def test_lon_lat_to_meters():
+    
+    dummy_craters = pd.read_csv(os.path.join(THIS_DIR, "sample_cluster.csv"), index_col=0)
+    xy = dummy_craters[["x", "y"]].copy()
+    
+    lat_center = 30
+    lon_center = 120
+    Rp = 3e6
+    
+    dummy_craters["lat"] = dummy_craters["x"] * 180 / (Rp * np.pi) + lat_center
+    dummy_craters["lon"] = dummy_craters["y"] * 180 / (Rp * np.pi * np.cos(lat_center * np.pi/180))\
+        + lon_center
+    
+    crater_tools.lon_lat_to_meters(dummy_craters, Rp)
+    pd.testing.assert_frame_equal(dummy_craters[["x", "y"]], xy)
+        
+    
+def convergence_analysis_ellipse():
+    
+    import matplotlib.pyplot as plt
+    dummy_craters = pd.read_csv(os.path.join(THIS_DIR, "sample_cluster.csv"), index_col=0)
+    crater_tools.plot_craters(dummy_craters, ellipse_color="blue")
+    plt.show()
+    
+    aspects, angles = list(), list()
+    iterations = np.array([1, 2, 3, 5, 8, 10, 15, 25, 40, 50, 60, 70, 80, 90, 100, 120, 150,
+                           250, 400, 600, 1000, 2000, 4000, 10000])
+    for iters in iterations:
+        aspect, _, components = crater_tools.ellipse(dummy_craters, bootstrap_samples=iters)
+        aspects.append(aspect)
+        angles.append(np.arctan2(components[1, 0], components[0, 0]))
+    
+    aspects = np.array(aspects)
+    angles = np.array(angles)
+    
+    plt.loglog(iterations[:-1], np.abs(aspects[:-1] - aspects[-1]), label="aspect")
+    plt.loglog(iterations[:-1], np.abs(angles[:-1] - angles[-1]), label="angle")
+    plt.xlabel("boostrap samples")
+    plt.ylabel("error")
+    plt.title("angle = {:.3f}, aspect = {:.3f}".format(angles[-1], aspects[-1]))
+    plt.legend()
+    plt.show()
+    
+
+if __name__ == "__main__":
+    test_effective_diameter()
+    test_dispersion()
+    test_ellipse()
+    test_f_n_larger_d_max_frac()
+    test_lon_lat_to_meters()
+    # convergence_analysis_ellipse()
