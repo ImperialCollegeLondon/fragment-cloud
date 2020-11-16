bf52a31f014c2b7797ac6d8bd7beb82c0dcc91be
Dominic Schwarz <dominic.schwarz@gmx.ch>
2020-11-16 14:44:58 +0100

    added docstrings to fcm.atmosphere module

--- fcm/atmosphere/_density_functions.py
+++ fcm/atmosphere/_density_functions.py
@@ -1,3 +1,4 @@
+"""Provides a couple of standard atmoshperic density tables"""
 __all__ = ["exponential", "US_standard_atmosphere", "static_martian_atmosphere"]
 
 import os
@@ -5,17 +6,73 @@ import math
 import numpy as np
 import pandas as pd
 
+from fcm.models import _check_number
+
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-def exponential(rho0, hmax=100, scale_height=8, hmin=0):
-    h = np.linspace(hmin, hmax, math.ceil((hmax - hmin) / scale_height) + 1)
+###################################################
+def exponential(rho0, hmax, scale_height, granularity=None, h0=0):
+    """Computes exponentially decaying atmoshperic density
+    rho(h) = rho0 * exp(-h / H)
+    
+    Parameters
+    ----------
+    rho0 : float
+        atmospheric density at altitude h0 (default: h0 = 0)
+        rho0 > 0
+    
+    hmax : float
+        altitude in [km] up to which to calculate the atmospheric density
+    
+    scale_height : float
+        height difference H for which rho(h + H) = rho(h) / e
+    
+    granularity : int, optional
+        number of points at which to calculate the atmospheric density
+        granularity >= 2
+        default : granularity = (hmax - hmin) / scale_height
+    
+    h0 : float, optional
+        altitude in [km] where rho(h0) = rho0
+        default = 0
+        h0 < hmax
+    
+    Returns
+    -------
+    pandas.Series
+        atmospheric density (kg/m^3)
+        index = altitude above 0 (km)
+    """
+    rho0 = _check_number(rho0, "rho0", lower_bound=0)
+    h0 = _check_number(h0, "h0")
+    hmax = _check_number(hmax, "hmax")
+    if not h0 < hmax:
+        raise ValueError("hmax ({:.2f}) must be larger than h0 ({:.2f})".format(hmax, h0))
+    
+    if granularity is None:
+        granularity = math.ceil((hmax - h0) / scale_height) + 1
+    else:
+        granularity = _check_number(granularity, "granularity", False, 2, True)
+    
+    h = np.linspace(h0, hmax, granularity)
     
     return pd.Series(data=rho0 * np.exp(-h / scale_height), index=h)
 
 
+###################################################
 def US_standard_atmosphere():
-    data = pd.read_csv(os.path.join(THIS_DIR, "AltitudeDensityTable.csv"), sep=" ", header=None, skiprows=6, index_col=0)
+    """Returns the US Standard Atmosphere table (1976),
+    National Aeronautics and Space Administration
+    
+    Returns
+    -------
+    pandas.Series
+        atmospheric density (kg/m^3)
+        index = altitude above sea level (km)
+    """
+    data = pd.read_csv(os.path.join(THIS_DIR, "AltitudeDensityTable.csv"),
+                       sep=" ", header=None, skiprows=6, index_col=0)
     
     data.columns = ["Atmospheric Density (kg/m3)", "H - scale height (m)"]
     data.index.name = "Altitude (km)"
@@ -24,8 +81,19 @@ def US_standard_atmosphere():
     return data["Atmospheric Density (kg/m3)"]
 
 
+###################################################
 def static_martian_atmosphere():
-    data = pd.read_csv(os.path.join(THIS_DIR, "Martian_Density.csv"), sep="\t", header=None, index_col=0)
+    """Returns example of a Martian atmospheric density. Intended for testing and debugging.
+    Use the fcm.atmosphere.martian_atmosphere_api() function for accurate data.
+    
+    Returns
+    -------
+    pandas.Series
+        atmospheric density (kg/m^3)
+        index = altitiude above MOLA_0 (km)
+    """
+    data = pd.read_csv(os.path.join(THIS_DIR, "Martian_Density.csv"),
+                       sep="\t", header=None, index_col=0)
     
     data = data[data.columns[0]]
     data.name = "Atmospheric Density (kg/m3)"
--- fcm/atmosphere/_mars_atm_api.py
+++ fcm/atmosphere/_mars_atm_api.py
@@ -1,3 +1,6 @@
+"""Module for accessing the website http://www-mars.lmd.jussieu.fr/mcd_python/, which provides
+height v. atmospheric density data for any pair of coordinates on Mars, for a given time stamp.
+"""
 __all__ = ["martian_atmosphere_api"]
 
 import re
@@ -7,50 +10,93 @@ import requests
 import jdcal
 import pandas as pd
 
-def _request_url(latitude, longitude, timestamp):
+from fcm.models import _check_number
+
+BASE_URL = "http://www-mars.lmd.jussieu.fr/mcd_python/"
+
+
+###################################################
+def martian_atmosphere_api(latitude, longitude, timestamp):
+    """Loads atmospheric density data for any coordinates on Mars for a given timestamp.
+    The timestamp is important, since the density data varies significantly with Martian seasons.
     
+    Parameters
+    ----------
+    latitiude : float
+        degrees North
+        -90 <= latitude <= 90
+    
+    longitude : float
+        degrees East
+        -180 < longitude <= 180
+        
+    timestamp : Union[datetime.date, datetime.datetime]
+        timestamp for which to request the data
+    
+    Returns
+    -------
+    pandas.Series
+        atmoshperic density (kg/m^3)
+        index = altitude above MOLA_0 (km)
+    """
+    latitude = _check_number(latitude, "latitude", True, -90, True, 90, True)
+    longitude = _check_number(longitude, "longitude", True, -180, False, 180, True)
     if not isinstance(timestamp, (datetime.date, datetime.datetime)):
         raise TypeError("timestamp must be a date or datetime object")
+    
+    url = _request_url(latitude, longitude, timestamp)
+    txt_url = _get_txt_url(url)
+    dataframe = _load_and_parse_txt_file(txt_url)
+    
+    dataframe.index *= 1e-3
+    dataframe.index.name = "altitude above MOLA_0 (km)"
+    
+    return dataframe.iloc[:, 0]
+
+
+###################################################
+def _request_url(latitude, longitude, timestamp):
+    """Converts latitude, longitude and timestamp into a request url to the website"""
     
     jdate = sum(jdcal.gcal2jd(timestamp.year, timestamp.month, timestamp.day))
 
     if isinstance(timestamp, datetime.datetime):
         jdate += timestamp.hour / 24 + timestamp.minute / (24*60) + timestamp.second / (24*3600)
     
-    url = "http://www-mars.lmd.jussieu.fr/mcd_python/cgi-bin/mcdcgi.py?"
+    url = BASE_URL + "cgi-bin/mcdcgi.py?"
     url += "&julian={:.5f}&latitude={:.9f}&longitude={:.9f}".format(jdate, latitude, longitude)
     url += "&altitude=all&zkey=2&var1=rho&colorm=jet"
     
     return url
 
 
-def _get_html_response(url, timeout=2):
-    
+###################################################
+def _get_txt_url(url, timeout=4, pattern=re.compile("txt/[a-f0-9]+.txt")):
+    """Sends GET request to url with timeout. Extracts url where data file can be downloaded from
+    the response, returns it.
+    """
     response = requests.get(url, timeout=timeout)
-    if response.status_code != 200:
-        raise RuntimeError("request failed with status code {:d}".format(response.status_code))
+    response.raise_for_status()
     
-    return response.text
-
-
-def _extract_txt_url(html, pattern=re.compile("txt/[a-f0-9]+.txt")):
-    
-    match = pattern.search(html)
+    match = pattern.search(response.text)
     if match is None:
-        raise ValueError("pattern not found")
+        raise ValueError("pattern not found in html response:\n{}".format(response.text))
 
-    return "http://www-mars.lmd.jussieu.fr/mcd_python/{}".format(match.group(0))
+    return BASE_URL + match.group(0)
 
 
+###################################################
 def _load_and_parse_txt_file(url, timeout=2):
+    """Loads csv file from url and converts it into a pandas.DataFrame"""
     
+    print("txt url:", url)
     buffer = io.BytesIO()
     with requests.get(url, stream=True, timeout=timeout) as r:
         r.raise_for_status()
         buffer.write(r.content)
     
     buffer.seek(0)
-    atmosphere = pd.read_csv(buffer, sep="\s+", header=None, index_col=0, comment="#")
+    atmosphere = pd.read_csv(buffer, sep=r"\s+", header=None, index_col=0, comment="#")
     buffer.close()
     
     assert atmosphere.shape[1] == 1,\
@@ -61,16 +107,3 @@ def _load_and_parse_txt_file(url, timeout=2):
     atmosphere.dropna(axis=0, how="any", inplace=True)
     
     return atmosphere
-    
-
-def martian_atmosphere_api(latitude, longitude, timestamp):
-    
-    url = _request_url(latitude, longitude, timestamp)
-    response = _get_html_response(url)
-    txt_url = _extract_txt_url(response)
-    dataframe = _load_and_parse_txt_file(txt_url)
-    
-    dataframe.index *= 1e-3
-    dataframe.index.name = "altitude above MOLA_0 (km)"
-    
-    return dataframe.iloc[:, 0]
