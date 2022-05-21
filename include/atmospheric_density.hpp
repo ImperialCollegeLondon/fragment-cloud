035e8372984c4813b1d1215fb4a47830ad9c18f5
Dominic Schwarz <dominic.schwarz@gmx.ch>
2022-05-21 13:25:35 +0100

    reduced code duplication by moving constructor of fcm::AtmosphericDensity into header file

--- include/atmospheric_density.hpp
+++ include/atmospheric_density.hpp
@@ -1,6 +1,9 @@
 #ifndef ATMOSPHERIC_DENSITY_HPP
 #define ATMOSPHERIC_DENSITY_HPP
 
+#include <algorithm>
+#include <cassert>
+#include <cmath>
 #include <vector>
 
 namespace fcm {
@@ -16,8 +19,19 @@ public:
      * @param height : height above sea level, in [m], in ascending order
      * @param density : air density at these heights, in [kg/m^3]
      */
-    AtmosphericDensity(const std::vector<double>& height, const std::vector<double>& density);
-    AtmosphericDensity(std::vector<double>&& height, const std::vector<double>& density);
+    template<class T, class U>
+    AtmosphericDensity(T&& height, const U& density) {
+        assert(height.size() == density.size());
+        assert(std::is_sorted(height.cbegin(), height.cend()));
+        assert(std::is_sorted(density.crbegin(), density.crend()));
+        assert(density.back() > 0);
+
+        this->height_ = std::forward<T>(height);
+        this->log_rho_ = std::vector<double>(density.size());
+
+        std::transform(density.cbegin(), density.cend(), this->log_rho_.begin(),
+                       [](const double rho){ return std::log(rho); });
+    }
 
     /**
      * @brief calculate air density at height h, in [kg/m^3]
@@ -26,7 +40,7 @@ public:
      * 
      * @param h : height above sea level, in [m]
      */
-    double calc(const double h) const noexcept;
+    double calc(double h) const noexcept;
 
     /**
      * @brief calculate scale height H at height h
@@ -37,7 +51,7 @@ public:
      * @param h : height above sea level, in [m]
      * @return double : scale height, in [m]
      */
-    double scale_height(const double h) const noexcept;
+    double scale_height(double h) const noexcept;
 
 private:
     std::vector<double> height_, log_rho_;
--- src/atmospheric_density.cpp
+++ src/atmospheric_density.cpp
@@ -1,39 +1,6 @@
 #include "atmospheric_density.hpp"
 
-#include <algorithm>
-#include <cassert>
-#include <vector>
-#include <cmath>
-
 using namespace fcm;
-
-AtmosphericDensity::AtmosphericDensity(const std::vector<double>& height,
-                                       const std::vector<double>& density) {
-    assert(height.size() == density.size());
-    assert(std::is_sorted(height.cbegin(), height.cend()));
-    assert(std::is_sorted(density.crbegin(), density.crend()));
-    assert(density.back() > 0);
-
-    this->height_ = height;
-    this->log_rho_ = std::vector<double>(density.size());
-
-    std::transform(density.cbegin(), density.cend(), this->log_rho_.begin(),
-                   [](const double rho){ return std::log(rho); });
-}
-
-AtmosphericDensity::AtmosphericDensity(std::vector<double>&& height,
-                                       const std::vector<double>& density) {
-    assert(height.size() == density.size());
-    assert(std::is_sorted(height.cbegin(), height.cend()));
-    assert(std::is_sorted(density.crbegin(), density.crend()));
-    assert(density.back() > 0);
-
-    this->height_ = std::move(height);
-    this->log_rho_ = std::vector<double>(density.size());
-
-    std::transform(density.cbegin(), density.cend(), this->log_rho_.begin(),
-                   [](const double rho){ return std::log(rho); });
-}
 
 inline auto _h_index(const std::vector<double>& sorted_vector, const double h) {
     return std::max(1L, std::distance(
@@ -43,10 +10,6 @@ inline auto _h_index(const std::vector<double>& sorted_vector, const double h) {
 
 double AtmosphericDensity::calc(const double h) const noexcept {
     const auto h_index = std::min(long(this->height_.size() - 1), _h_index(this->height_, h));
-    // const auto h_index = _h_index(this->height_, h);
-    // if (h_index == this->height_.size()) {
-    //     return 0;
-    // }
     if (std::abs(h - this->height_[h_index]) < 1e-1) {
         return std::exp(this->log_rho_[h_index]);
     }
